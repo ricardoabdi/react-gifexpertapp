21d5396d90f251b9cc0e0e5b552b2bab277f214c
Ricardo Gutierrez <ricardoabdielg@gmail.com>
2021-01-28 15:50:06 -0500

    docs agregado

---
+++ docs/precache-manifest.5a38b3f0e396bd6747f31676132c6780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdd841ddc321b16c91368d998007544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "879660485be1dd2a2cd7",
+    "url": "/static/css/main.2eb9737e.chunk.css"
+  },
+  {
+    "revision": "9136da3ca0c27c7becf0",
+    "url": "/static/js/2.f1cad26a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1cad26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879660485be1dd2a2cd7",
+    "url": "/static/js/main.3a6919c6.chunk.js"
+  },
+  {
+    "revision": "21647c64104c6f40a2c0",
+    "url": "/static/js/runtime-main.199852c7.js"
+  }
+]);
